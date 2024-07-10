54f3c7c91fc6a5a8ed92741a46df8e17e83da268
Jalees Ahmed <jq2692@att.com>
2024-07-10 15:23:04 +0530

    fixing

--- providers.tf
+++ providers.tf
@@ -2,15 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      source = "hashicorp/net"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
-}
-
-provider "net" {
   region  = "us-west-2"
 }
